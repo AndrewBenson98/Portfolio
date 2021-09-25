f102b2d2cdd20cec963b3fa72219ccf3c9bd7680
AndrewBenson98 <andrewbenson123@gmail.com>
2021-09-25 10:24:30 -0400

    updated settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') =='True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','arbenson-portfolio.herokuapp.com']
 
 
 # Application definition
@@ -126,11 +126,11 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_URL = '/static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
